5d484f6995a0fce96f3afb6a3087230f1fa6e68f
Kirti Arora <Kirti-1@users.noreply.github.com>
2025-04-26 01:51:21 +0530

    added cover photo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import axios from 'axios';
 import {useState} from 'react';
+import coverPic from './../assets/cover_pic.png';
 
 function HomePage() {
   const [notes, setNotes] =  useState([]) 
@@ -27,17 +28,24 @@ function HomePage() {
         </header>
       </div>
       <hr/>
-      <Row xs={1} md={2} className="g-4">
+      <Row xs={1} md={3} className="g-4">
         {notes.map((note, idx) => (
             // {console.log(notes)}
           <Col key={idx}>
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Img variant="top" width="100" height="200" src={coverPic} />
               <Card.Body>
                 <Card.Title>{note.subjectLine}</Card.Title>
-                <Card.Text>
+                {/* React escapes HTML inside curly braces by default to prevent XSS attacks, 
+                so you're seeing <b>bold</b> (as text), not bolded output.
+                Only use this if you're sure the HTML is safe (e.g., 
+                it's sanitized or from a trusted source). Otherwise, it can expose your app to security risks.
+                 */}
+                <Card.Text dangerouslySetInnerHTML={{ __html: note.body }} />
+
+                {/* <Card.Text>
                  {note.body}
-                </Card.Text>
+                </Card.Text> */}
               </Card.Body>
             </Card>
           </Col>
